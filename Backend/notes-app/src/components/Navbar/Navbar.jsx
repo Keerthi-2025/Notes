20e8cdd8759c7c54e8d9bc54bc1c7dd21488cb58
Keerthi <rhkeerthi3@gmail.com>
2026-04-29 14:58:31 +0530

    modified Login page

--- Backend/notes-app/src/components/Navbar/Navbar.jsx
+++ Backend/notes-app/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-white px-6 py-2 flex items-center justify-between'>
+    <div className='bg-white px-6 py-2 flex items-center justify-between drop-shadow-2xl'>
         <h2 className='text-xl font-medium py-2'>Notes</h2>
     </div>
   )
--- Backend/notes-app/src/pages/Login/Login.jsx
+++ Backend/notes-app/src/pages/Login/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import {Link} from 'react-router-dom'
 
 export const Login = () => {
   return (
     <>
     <Navbar/>
+
+    <div className='flex items-center justify-center mt-28'>
+      <div className='w-96 border rounded bg-white px-7 py-10'>
+        <form>
+          <h4 className='text-2xl mb-7'>Login </h4>
+            <input type='text' placeholder='Email' className='input-box'/>
+
+            <button  type="submit" className='btn-primary'>Login</button>
+
+            <p>Not regsitered yet?{" "}
+              <Link to="/signup" className="font-medium underline">Create and account</Link>
+            </p>
+         
+        </form>
+      </div>
+    </div>
     </>
   )
 }
