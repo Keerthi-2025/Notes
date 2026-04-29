e29e18a3a0870eb416f34351296de2361f44872f
Keerthi <rhkeerthi3@gmail.com>
2026-04-29 14:35:43 +0530

    added broswer router

--- Backend/notes-app/src/App.jsx
+++ Backend/notes-app/src/App.jsx
@@ -3,12 +3,22 @@ import Home from './pages/Home/Home'
 import { Login } from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+
+const routes =(
+  <Router>
+    <Routes>
+      <Route path='/dashboard'  element={<Home/>}/>
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>}></Route>
+    </Routes>
+  </Router>
+)
+
 const App = () => {
   return (
     <div>
-      <Home/>
-      <Login/>
-      <Signup/>
+    {routes}
     </div>
   )
 }
