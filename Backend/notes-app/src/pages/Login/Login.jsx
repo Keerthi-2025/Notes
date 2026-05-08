e5b5c359414ad2ca8b9b256d764a50e6839ab30f
Keerthi <rhkeerthi3@gmail.com>
2026-05-08 21:14:54 +0530

    modified Login file

--- Backend/notes-app/src/pages/Login/Login.jsx
+++ Backend/notes-app/src/pages/Login/Login.jsx
@@ -8,13 +8,18 @@ export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const handleLogin = async (e) =>{
+    e.preventDeafault();
+  }
+  
   return (
     <>
     <Navbar/>
 
     <div className='flex items-center justify-center mt-28'>
       <div className='w-96 border rounded bg-white px-7 py-10'>
-        <form>
+        <form onSubmit={handleLogin}>
           <h4 className='text-2xl mb-7'>Login </h4>
             {/* <input type='text' placeholder='Email' className='input-box'/> */}
 
@@ -25,7 +30,9 @@ export const Login = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}/>
 
-            <PasswordInput />
+            <PasswordInput 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}/>
 
             <button  type="submit" className='btn-primary'>Login</button>
 
