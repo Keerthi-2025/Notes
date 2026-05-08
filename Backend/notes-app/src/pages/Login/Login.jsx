f259d9138cbbed4331a247ed96a0e9e7c69d2836
Keerthi <rhkeerthi3@gmail.com>
2026-05-08 21:11:49 +0530

    modified Login file

--- Backend/notes-app/src/pages/Login/Login.jsx
+++ Backend/notes-app/src/pages/Login/Login.jsx
@@ -16,9 +16,16 @@ export const Login = () => {
       <div className='w-96 border rounded bg-white px-7 py-10'>
         <form>
           <h4 className='text-2xl mb-7'>Login </h4>
-            <input type='text' placeholder='Email' className='input-box'/>
+            {/* <input type='text' placeholder='Email' className='input-box'/> */}
 
-            <PasswordInput/>
+            < input
+            type='text'
+            placeholder='Email'
+            className='input-box'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
+
+            <PasswordInput />
 
             <button  type="submit" className='btn-primary'>Login</button>
 
