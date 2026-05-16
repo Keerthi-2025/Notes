338f8294a1b23cde225b6c3778d9e69a26601c54
Keerthi <rhkeerthi3@gmail.com>
2026-05-16 23:13:58 +0530

    completed signup page

--- Backend/notes-app/src/pages/Signup/Signup.jsx
+++ Backend/notes-app/src/pages/Signup/Signup.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../../components/Navbar/Navbar';
+import PasswordInput from '../../components/Input/PasswordInput';
+import { Link } from 'react-router-dom';
+import { validateEmail } from '../../utils/helper';
 
 const Signup = () => {
 
+  const [name,setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
 const handleSignUp = async(e)=>{
       e.preventDefault();
+
+
+      if(!name){
+        setError("Please enter your name");
+        return
+      }
+
+      if(!validateEmail(email)){
+        setError("Please enter a valid email address");
+        return
+      }
+
+      if(!password){
+        setError("Please enter the password");
+        return
+      }
+      setError('')
     };
 
 
@@ -17,7 +43,37 @@ const handleSignUp = async(e)=>{
     <div className='flex items-center justify-center mt-28'>
       <div className='w-96 border rounded bg-white px-7 py-10'>
         <form onSubmit={handleSignUp}>
-          <h4 className='text-2xl mb-7'>Login </h4>
+          <h4 className='text-2xl mb-7'>SignUp </h4>
+
+          <input
+          type='text'
+          placeholder='Name'
+          className='input-box'
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+          />
+
+          <input
+          type='text'
+          placeholder='Email'
+          className='input-box'
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}/>
+
+          <PasswordInput
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+          />
+
+
+          <button type='submit' className='btn-primary'>Create an Account</button>
+
+          {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+         
+          <p>Already have an Account ? {" "}
+            <Link to="/login" className='font-medium underline'>Login</Link>
+          </p>
+
           </form>
           </div>
           </div>
