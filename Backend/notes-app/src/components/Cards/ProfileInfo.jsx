3ee17759b827678ef4d76ac7677dc40c3ac696c2
Keerthi <rhkeerthi3@gmail.com>
2026-05-16 23:46:14 +0530

    added logout function

--- Backend/notes-app/src/components/Cards/ProfileInfo.jsx
+++ Backend/notes-app/src/components/Cards/ProfileInfo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import {getInitials} from "../../utils/helper";
 
 const ProfileInfo = ({onLogout}) => {
   return (
     <div className='flex items-center gap-3'>
-        <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-200'>TU</div>
+        <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-200'>{getInitials("jon william")}</div>
 
         <div>
-            <p className='text-sm font-medium'>William</p>
+            <p className='text-sm font-medium'>jon william</p>
             <button className='text-sm  text-slate-700 underline' onClick={onLogout}>Logout</button>
 
         </div>
--- Backend/notes-app/src/components/Navbar/Navbar.jsx
+++ Backend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import ProfileInfo from '../Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+const navigate = useNavigate();
+
+const onLogout =() =>{
+  navigate("/login");
+}
+
+
   return (
     <div className='bg-white px-6 py-2 flex items-center justify-between drop-shadow-2xl'>
         <h2 className='text-xl font-medium py-2'>Notes</h2>
 
-        <ProfileInfo/>
+        <ProfileInfo  onLogout={onLogout}/>
     </div>
   )
 }
