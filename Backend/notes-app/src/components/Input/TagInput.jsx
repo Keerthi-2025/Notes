12d9e721b5f519a5e6ccf1d1c5bb642671ead009
Keerthi <rhkeerthi3@gmail.com>
2026-05-18 23:12:06 +0530

    added design to TagInput page

--- Backend/notes-app/src/components/Input/TagInput.jsx
+++ Backend/notes-app/src/components/Input/TagInput.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const TagInput = () => {
+import { MdAdd, MdClose } from 'react-icons/md'
+
+const TagInput = ({tags, setTags}) => {
   return (
-    <div>TagInput</div>
+    <div>
+        <div className='flex items-center gap-4 mt-3'>
+            <input type='text' placeholder='Add Tags' className='text-sm bg-transparent px3 py-2 rounded outline-none'/>
+
+            <button className='w-6.5 h-8 items-center justify-center rounded border border-blue-700 hover:bg-blue-700'><MdAdd className='text-2xl text-blue-700 hover:text-white'/></button>
+        </div>
+    </div>
   )
 }
 
-export default TagInput
+export default TagInput 
